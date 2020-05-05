ba9191c85df4a0665667a53d3aaca2d8472df6b3
Ian D. Miller <iandouglas96@gmail.com>
2020-05-05 17:47:34 -0400

    Added GMM computation

--- include/top_down_render/particle_filter.h
+++ include/top_down_render/particle_filter.h
@@ -8,6 +8,7 @@
 #include <ros/ros.h>
 #include <Eigen/Dense>
 #include <opencv2/core/core.hpp>
+#include <opencv2/ml/ml.hpp>
 #include <opencv2/imgproc/imgproc.hpp> //cv::circle
 
 class ParticleFilter {
@@ -17,9 +18,11 @@ class ParticleFilter {
     void update(std::vector<Eigen::ArrayXXf> &top_down_scan, 
                 std::vector<Eigen::ArrayXXf> &top_down_geo, float res);
     void computeCov(Eigen::Matrix2f &cov);
+    void computeGMM(std::vector<Eigen::Vector3f> &means, std::vector<Eigen::Matrix3f> &covs);
     void visualize(cv::Mat &img);
   private:
     int num_particles_;
+    int num_gaussians_;
     std::vector<std::shared_ptr<StateParticle>> particles_;
     std::vector<std::shared_ptr<StateParticle>> new_particles_;
 
--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -5,6 +5,7 @@ ParticleFilter::ParticleFilter(int N, float width, float height, TopDownMapPolar
   gen_ = new std::mt19937(rd());
 
   num_particles_ = N;
+  num_gaussians_ = 1;
   map_ = map;
   width_ = width;
   height_ = height;
@@ -47,10 +48,22 @@ void ParticleFilter::update(std::vector<Eigen::ArrayXXf> &top_down_scan,
   max_likelihood_particle_ = particles_[max_weight];
 
   ROS_INFO_STREAM("particle reweighting complete");
+  
+  //GMM
+  std::vector<Eigen::Vector3f> means;
+  std::vector<Eigen::Matrix3f> covs;
+  computeGMM(means, covs);
+  for (auto m : means) {
+    ROS_INFO_STREAM(m);
+  }
 
-  Eigen::Matrix2f cov;
-  computeCov(cov);
-  num_particles_ = std::min(static_cast<int>(sqrt(cov(0,0))*sqrt(cov(1,1)))*5, 10000); //Approximation of area of cov ellipse
+  //should really use eigenvalues instead of diagonal
+  num_particles_ = 0;
+  for (auto cov : covs) {
+    Eigen::Vector2cf eig = cov.block<2,2>(0,0).eigenvalues(); //complex vector
+    num_particles_ += static_cast<int>(sqrt(eig[0].real())*sqrt(eig[1].real()))*5; //Approximation of area of cov ellipse
+  }
+  num_particles_ = std::min(std::max(num_particles_, 10), 10000); //bounds
   ROS_INFO_STREAM(num_particles_ << " particles");
 
   //resize num_particles_
@@ -88,6 +101,60 @@ void ParticleFilter::computeCov(Eigen::Matrix2f &cov) {
     cov += state*state.transpose();
   }
   cov /= particles_.size()-1;
+}
+
+void ParticleFilter::computeGMM(std::vector<Eigen::Vector3f> &means, std::vector<Eigen::Matrix3f> &covs) {
+  //We do this recursively: GMM in euclidean space, and then GMM on each cluster in theta space
+  cv::Ptr<cv::ml::EM> em = cv::ml::EM::create();
+  em->setClustersNumber(num_gaussians_);
+  em->setCovarianceMatrixType(cv::ml::EM::COV_MAT_GENERIC);
+
+  //Build sample array
+  int num_samples = std::min(500, static_cast<int>(particles_.size()));
+  cv::Mat samples = cv::Mat(num_samples, 2, CV_64F);
+  for (int i=0; i<num_samples; i+=1) {
+    samples.at<double>(i, 0) = particles_[i*particles_.size()/num_samples]->mlState()[0];
+    samples.at<double>(i, 1) = particles_[i*particles_.size()/num_samples]->mlState()[1];
+  }
+
+  cv::Mat likelihoods, labels, cluster_means;
+  em->trainEM(samples, likelihoods, labels);
+  float likelihood_mean = cv::mean(likelihoods)[0];
+
+  int dir = 0;
+  //try increasing
+  if (num_gaussians_*50 < num_particles_) {
+    em->setClustersNumber(num_gaussians_ + 1);
+    em->trainEM(samples, likelihoods, labels);
+    if (likelihood_mean + 0.2 < cv::mean(likelihoods)[0]) {
+      dir = 1;
+    }
+  }
+  //try decr
+  if (num_gaussians_ > 1) {
+    em->setClustersNumber(num_gaussians_ - 1);
+    em->trainEM(samples, likelihoods, labels);
+    if (likelihood_mean - 0.2 < cv::mean(likelihoods)[0]) {
+      dir = -1;
+    }
+  }
+
+  num_gaussians_ += dir;
+  em->setClustersNumber(num_gaussians_);
+  em->trainEM(samples, likelihoods, labels);
+  cluster_means = em->getMeans();
+  std::vector<cv::Mat> cluster_covs;
+  em->getCovs(cluster_covs);
+
+  //Convert to Eigen by manually copying elements
+  for (int i=0; i<cluster_covs.size(); i++) {
+    means.push_back(Eigen::Vector3f(cluster_means.at<double>(i,0), cluster_means.at<double>(i,1), 0));
+    Eigen::Matrix3f cov;
+    cov << cluster_covs[i].at<double>(0,0), cluster_covs[i].at<double>(0,1), 0,
+           cluster_covs[i].at<double>(1,0), cluster_covs[i].at<double>(1,1), 0,
+           0, 0, 1;
+    covs.push_back(cov);
+  }
 }
 
 void ParticleFilter::visualize(cv::Mat &img) {
